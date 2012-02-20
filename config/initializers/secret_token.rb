feae594df48e3f97e065683ba62b6f25af54da5e
designhawg <designhawg@mac.com>
2012-02-20 00:05:42 -0600

    Demo 3 Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hartl3::Application.config.secret_token = 'e3d907ab3b0956c44dd5855341d8ffa0c87b7f2b061bef8b1b8077ca6761cf964589e124671fed64d7d0442b83999cf94d69b3bc994cf6e4d2e339e87ac7fcf3'
